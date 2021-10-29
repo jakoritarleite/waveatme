be0080d964e20300da692be75e603b587888e2a5
Joao Koritar <jakoritarleite@gmail.com>
2021-10-29 10:08:01 -0300

    feat: create function to add wave

--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,21 @@ const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  console.log('Contract deployed', waveContract.address);
+  console.log('Contract addy:', waveContract.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log('Total waves', waveCount.toNumber());
+
+  let waveTxn = await waveContract.wave("That's a message uwu");
+  await waveTxn.wait();
+
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait();
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
